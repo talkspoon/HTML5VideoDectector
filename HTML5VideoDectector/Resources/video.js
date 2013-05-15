b704886521306e9d7e571b876d1b575c6c62eacc
AlanZ <tozhufeng@gmail.com>
2013-05-15 17:05:54 +0800

    optimized

--- HTML5VideoDectector/Resources/video.js
+++ HTML5VideoDectector/Resources/video.js
@@ -1,5 +1,13 @@
-(function() {
-    var h5vd_html5videos = document.getElementsByTagName("video");
-    location.href = "h5vd://h5vd.com/parse?video_src=" + encodeURIComponent(h5vd_html5videos[0].src);
-})();
-
+var h5vd_init = function() {
+    var h5vd = document.createElement('script');
+    h5vd.type = 'text/javascript';
+    h5vd.async = true;
+    h5vd.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.alanz.org/h5vd/detector.js';
+    var s = document.getElementsByTagName('script')[0];
+    if (s) {
+        s.parentNode.insertBefore(h5vd, s);
+    } else {
+        setTimeout(function(){h5vd_init();}, 100);
+    }
+};
+h5vd_init();
